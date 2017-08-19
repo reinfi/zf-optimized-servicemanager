aea49ac7190b1033ad24c4497f2d3934f1cf1e09
Martin Reinfandt <reinfandt@z-em.de>
2017-08-19 17:43:13 +0200

    add module class to replace service manager

---
+++ src/Module.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Reinfi\OptimizedServiceManager;
+
+use Reinfi\OptimizedServiceManager\Service\OptimizerService;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\InitProviderInterface;
+use Zend\ModuleManager\ModuleEvent;
+use Zend\ModuleManager\ModuleManagerInterface;
+
+/**
+ * @package Reinfi\DependencyInjection
+ */
+class Module implements ConfigProviderInterface, InitProviderInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getConfig(): array
+    {
+        return require __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * @param ModuleManagerInterface $manager
+     */
+    public function init(ModuleManagerInterface $manager)
+    {
+        $manager->getEventManager()->attach(
+            ModuleEvent::EVENT_LOAD_MODULES_POST,
+            [
+                $this,
+                'replaceServiceManager',
+            ],
+            PHP_INT_MAX
+        );
+    }
+
+    /**
+     * @param ModuleEvent $event
+     */
+    public function replaceServiceManager(ModuleEvent $event)
+    {
+        $container = $event->getParam('ServiceManager');
+
+        if (class_exists(OptimizerService::SERVICE_MANAGER_FQCN)) {
+            $managerClass = OptimizerService::SERVICE_MANAGER_CLASSNAME;
+
+            $event->setParam('ServiceManager', new $managerClass($container));
+        }
+    }
+}
