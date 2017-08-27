b4d5eec2a51a86cc37e357e110447c35471ef0f4
Martin Reinfandt <reinfandt@z-em.de>
2017-08-27 17:24:01 +0200

    add missing route option

--- config/module.config.php
+++ config/module.config.php
@@ -27,7 +27,7 @@ return [
             'routes' => [
                 'reinfi-di-cache-warmup' => [
                     'options' => [
-                        'route'    => 'reinfi:optimize service-manager [--with-initializers] [--canonicalize-names]',
+                        'route'    => 'reinfi:optimize service-manager [--with-initializers] [--canonicalize-names] [--try-autowire]',
                         'defaults' => [
                             'controller' => \Reinfi\OptimizedServiceManager\Controller\GenerateController::class,
                             'action'     => 'index',
